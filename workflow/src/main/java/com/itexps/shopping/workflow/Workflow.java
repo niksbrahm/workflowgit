afb50c49373889c44bd5fec4ca1118b120b03a85
niksbrahm <bshainiweb@gmail.com>
2025-03-18 20:22:55 -0500

    initial version

---
+++ workflow/src/main/java/com/itexps/shopping/workflow/Workflow.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.itexps.shopping.workflow;
+
+/**
+ *
+ * @author niks_
+ */
+public class Workflow {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
